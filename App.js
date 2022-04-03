9490325cb61b6a49e057ba7f209482758bbaa6e0
Moataz Mohammady <moataz.mohammady@gmail.com>
2022-04-03 17:30:56 +0200

    add expo build:ios (#2)
    * add expo build:ios
    
    * get uri from app.js file
    
    * update icons

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { WebView } from "react-native-webview";
 
 export default function App() {
-  return <WebView source={{ uri: "https://store.arabhardware.net/en" }} />;
+  const uri = "https://store.arabhardware.net/";
+  return <WebView source={{ uri }} />;
 }
