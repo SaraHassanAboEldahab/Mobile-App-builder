0ab3db47019968b2ad01cda2d9c16f105f0c56cd
Moataz <moataz@wuilt.com>
2022-04-03 16:56:30 +0200

    add expo build:ios

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { WebView } from "react-native-webview";
 
 export default function App() {
