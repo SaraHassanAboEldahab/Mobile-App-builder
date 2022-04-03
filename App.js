ee849b3ff061f623bff6387cef28069a23b5e45d
SaraHassanAboEldahab <47996159+SaraHassanAboEldahab@users.noreply.github.com>
2022-04-03 04:15:01 +0200

    remove comments

--- App.js
+++ App.js
@@ -5,12 +5,3 @@ import { WebView } from "react-native-webview";
 export default function App() {
   return <WebView source={{ uri: "https://store.arabhardware.net/en" }} />;
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "gray",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
