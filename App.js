a5ed6d1c3f072ab34ebdfbd670a38df2ff7ffc6c
SaraHassanAboEldahab <47996159+SaraHassanAboEldahab@users.noreply.github.com>
2022-04-06 17:50:18 +0200

    try

---
+++ App.js
@@ -0,0 +1,8 @@
+import { WebView } from "react-native-webview";
+import * as core from "@actions/core";
+
+export default function App() {
+  const uri = core.getInput("storeLink", { required: true });
+  // const uri = "https://store.arabhardware.net/";
+  return <WebView source={{ uri }} />;
+}
--- index.js
+++
@@ -1,8 +0,0 @@
-import { WebView } from "react-native-webview";
-import * as core from "@actions/core";
-
-export default function App() {
-  const uri = core.getInput("storeLink", { required: true });
-  // const uri = "https://store.arabhardware.net/";
-  return <WebView source={{ uri }} />;
-}
