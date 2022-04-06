900e7bf3d4d98c326b2e470661c83825767df0d6
SaraHassanAboEldahab <47996159+SaraHassanAboEldahab@users.noreply.github.com>
2022-04-06 20:39:30 +0200

    try

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { WebView } from "react-native-webview";
 import * as core from "@actions/core";
 
 export default function App() {
-  // const uri = core.getInput("storeLink", { required: true });
-  const uri = "https://store.arabhardware.net/";
+  const uri = core.getInput("storeLink", { required: true });
+  // const uri = "https://store.arabhardware.net/";
   return <WebView source={{ uri }} />;
 }
