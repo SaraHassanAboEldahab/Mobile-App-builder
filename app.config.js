9ab6c298fd9ff02ed442d1040641d570bc13d271
Moataz <moataz@wuilt.com>
2022-04-07 21:45:06 +0200

    fix Constants

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default ({ config }) => {
   config.splash.image = process.env.image;
   config.android.adaptiveIcon.foregroundImage = process.env.adaptiveIcon;
   config.android.package = `${process.env.bundleIdentifier}.App`;
-  config.iso.bundleIdentifier = `${process.env.bundleIdentifier}.App`;
+  config.ios.bundleIdentifier = `${process.env.bundleIdentifier}.App`;
   return {
     ...config,
     extra: {
