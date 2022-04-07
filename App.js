9d3d0d2a738f1490f095c79dd29d1f6d219249b9
Moataz <moataz@wuilt.com>
2022-04-07 18:07:17 +0200

    move Constants

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
+import Constants from "expo-constants";
 import { WebView } from "react-native-webview";
 import appData from "./app-data.json";
+
+Constants.manifest.extra.icon === process.env.icon;
+Constants.manifest.extra.splash.image === process.env.plash;
+Constants.manifest.extra.android.package ===
+  process.env.bundleIdentifier + ".APP";
+Constants.manifest.extra.ios.bundleIdentifier ===
+  process.env.bundleIdentifier + ".APP";
+Constants.manifest.extra.android.adaptiveIcon.foregroundImage ===
+  process.env.adaptiveIcon;
 
 export default function App() {
   const uri = appData?.url;
--- app.config.js
+++ app.config.js
@@ -1,13 +1,3 @@
-import Constants from "expo-constants";
-Constants.manifest.extra.icon === process.env.icon;
-Constants.manifest.extra.splash.image === process.env.plash;
-Constants.manifest.extra.android.package ===
-  process.env.bundleIdentifier + ".APP";
-Constants.manifest.extra.ios.bundleIdentifier ===
-  process.env.bundleIdentifier + ".APP";
-Constants.manifest.extra.android.adaptiveIcon.foregroundImage ===
-  process.env.adaptiveIcon;
-
 export default {
   name: process.env.appName,
   version: process.env.MY_CUSTOM_PROJECT_VERSION || "1.0.0",
