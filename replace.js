16fbca6261b338751c3b38dc7d63a4759d5c75d1
Moataz <moataz@wuilt.com>
2022-04-07 17:40:48 +0200

    use SpicyPizza/create-envfile@v1

--- replace.js
+++ replace.js
@@ -13,6 +13,10 @@ fs.readFile(file, "utf8", function (err, data) {
     .replace(/.\/assets\/icon.png/g, process.env.icon)
     .replace(/.\/assets\/plash.png/g, process.env.plash)
     .replace(/.\/assets\/adaptive-icon.png/g, process.env.adaptiveIcon);
+
+  console.log("\n".repeat(5));
+  console.log("************ result **************");
+  console.log(JSON.stringify(result, null, 4));
   fs.writeFile(file, result, "utf8", function (err) {
     if (err) return console.log(err);
   });
