0b8de334b8b3830fd4b7e87d95c2613bc890d627
Moataz <moataz@wuilt.com>
2022-04-07 21:16:19 +0200

    fix Constants

--- App.js
+++ App.js
@@ -2,11 +2,11 @@ import Constants from "expo-constants";
 import { WebView } from "react-native-webview";
 import appData from "./app-data.json";
 
-Constants.manifest.extra.icon === process.env.icon;
-Constants.manifest.extra.splash.image === process.env.plash;
-Constants.manifest.extra.android.package === process.env.bundleIdentifier;
-Constants.manifest.extra.ios.bundleIdentifier === process.env.bundleIdentifier;
-Constants.manifest.extra.android.adaptiveIcon.foregroundImage ===
+Constants.manifest.extra.icon = process.env.icon;
+Constants.manifest.extra.splash.image = process.env.plash;
+Constants.manifest.extra.android.package = `${process.env.bundleIdentifier}.App`;
+Constants.manifest.extra.ios.bundleIdentifier = `${process.env.bundleIdentifier}.App`;
+Constants.manifest.extra.android.adaptiveIcon.foregroundImage =
   process.env.adaptiveIcon;
 
 export default function App() {
