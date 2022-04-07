ba4350b5c282f3726eff20feaaa55b976b492f38
SaraHassanAboEldahab <47996159+SaraHassanAboEldahab@users.noreply.github.com>
2022-04-07 03:04:37 +0200

    try

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { WebView } from "react-native-webview";
 import * as core from "@actions/core";
+import github from "@actions/github";
 
 export default function App() {
   const uri = core.getInput("storeLink", { required: true });
