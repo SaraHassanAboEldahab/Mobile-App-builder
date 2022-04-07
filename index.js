7baee8615f7ec18284be31ccf4044b55436dda0a
SaraHassanAboEldahab <47996159+SaraHassanAboEldahab@users.noreply.github.com>
2022-04-07 03:17:06 +0200

    try

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+import * as core from "@actions/core";
+import github from "@actions/github";
 
 const nameToGreet = core.getInput("storeLink");
 console.log(`Hello ${nameToGreet}!`);
