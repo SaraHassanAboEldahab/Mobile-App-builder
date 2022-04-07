1cf2ed086370ac87d9ffb9b832e9f4e3ec7484bd
Moataz <moataz@wuilt.com>
2022-04-07 21:07:21 +0200

    fix Constants

--- app.config.js
+++ app.config.js
@@ -1,4 +1,8 @@
 export default {
   name: process.env.appName,
   version: process.env.MY_CUSTOM_PROJECT_VERSION || "1.0.0",
+  extra: {
+    bundleIdentifier: process.env.bundleIdentifier,
+    package: process.env.bundleIdentifier + ".App",
+  },
 };
