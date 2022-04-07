ff219721c4c0ac333554f9603514c8cd7c053303
Moataz <moataz@wuilt.com>
2022-04-07 12:04:44 +0200

    fix read url

--- App.js
+++ App.js
@@ -2,6 +2,6 @@ import { WebView } from "react-native-webview";
 import appData from "./app-data.json";
 
 export default function App() {
-  const uri = appData?.expo?.url;
+  const uri = appData?.url;
   return <WebView source={{ uri }} />;
 }
