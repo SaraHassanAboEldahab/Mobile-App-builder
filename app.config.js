7c0747a4dd1baec13c53d628f7aa7c0105ae271b
Moataz <moataz@wuilt.com>
2022-04-08 11:11:04 +0200

    set bundleIdentifier

--- app.config.js
+++ app.config.js
@@ -5,6 +5,7 @@ const slug = process.env.SLUG;
 const icon = process.env.ICON;
 const splash = process.env.SPLASH;
 const adaptiveIcon = process.env.ADAPTIVE_ICON;
+const bundleIdentifier = process.env.BUNDLE_IDENTIFIER;
 const url = process.env.URL || "https://wuilt.com/";
 
 export default ({ config }) => {
@@ -23,6 +24,11 @@ export default ({ config }) => {
         ...config.android.adaptiveIcon,
         foregroundImage: adaptiveIcon,
       },
+      package: bundleIdentifier,
+    },
+    ios: {
+      ...config.ios,
+      bundleIdentifier,
     },
     extra: {
       url,
