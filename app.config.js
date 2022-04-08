5484da96ee67739ccc57cdabc2b150a0d566a27a
Moataz <moataz@wuilt.com>
2022-04-08 03:17:26 +0200

    fix read env

--- app.config.js
+++ app.config.js
@@ -3,26 +3,26 @@ require("dotenv").config();
 export default ({ config }) => {
   return {
     ...config,
-    name: process.env.appName,
-    slug: process.env.slug,
-    icon: process.env.icon,
+    name: process.env.APPNAME,
+    slug: process.env.SLUG,
+    icon: process.env.ICON,
     splash: {
       ...config.splash,
-      image: process.env.image,
+      image: process.env.SPLASH,
     },
     android: {
       ...config.android,
-      package: `${process.env.bundleIdentifier}.App`,
+      package: `${process.env.BUNDLEIDENTIFIER}.App`,
       adaptiveIcon: {
         ...config.android.adaptiveIcon,
-        foregroundImage: process.env.adaptiveIcon,
+        foregroundImage: process.env.ADAPTIVEICON,
       },
     },
     ios: {
-      bundleIdentifier: `${process.env.bundleIdentifier}.App`,
+      bundleIdentifier: `${process.env.BUNDLEIDENTIFIER}.App`,
     },
     extra: {
-      url: process.env.url,
+      url: process.env.URL,
     },
   };
 };
