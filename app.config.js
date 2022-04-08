78b9bfca0821205cf58ba77a78ce78e711ee8108
Moataz <moataz@wuilt.com>
2022-04-08 04:51:41 +0200

    use actions/core

--- app.config.js
+++ app.config.js
@@ -1,28 +1,34 @@
-require("dotenv").config();
-
+const core = require("@actions/core");
+const name = core.getInput("appName", { required: true });
+const slug = core.getInput("slug", { required: true });
+const icon = core.getInput("icon", { required: true });
+const image = core.getInput("image", { required: true });
+const bundleIdentifier = core.getInput("bundleIdentifier", { required: true });
+const adaptiveIcon = core.getInput("adaptiveIcon", { required: true });
+const url = core.getInput("url", { required: true });
 export default ({ config }) => {
   return {
     ...config,
-    name: process.env.appName,
-    slug: process.env.slug,
-    icon: process.env.icon,
+    name,
+    slug,
+    icon,
     splash: {
       ...config.splash,
-      image: process.env.image,
+      image,
     },
     android: {
       ...config.android,
-      package: `${process.env.bundleIdentifier}.App`,
+      package: `${bundleIdentifier}.App`,
       adaptiveIcon: {
         ...config.android.adaptiveIcon,
-        foregroundImage: process.env.adaptiveIcon,
+        foregroundImage: adaptiveIcon,
       },
     },
     ios: {
-      bundleIdentifier: `${process.env.bundleIdentifier}.App`,
+      bundleIdentifier: `${bundleIdentifier}.App`,
     },
     extra: {
-      url: process.env.url,
+      url,
     },
   };
 };
