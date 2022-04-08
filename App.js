a508eebda910b0e48ec1adbc39ecefc10beea732
Moataz <moataz@wuilt.com>
2022-04-08 12:56:09 +0200

    handle offline

--- App.js
+++ App.js
@@ -1,6 +1,37 @@
 import Constants from "expo-constants";
 import { WebView } from "react-native-webview";
-export default function App() {
+
+import { useNetInfo } from "@react-native-community/netinfo";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+const App = () => {
+  const netInfo = useNetInfo();
+  if (netInfo.type !== "unknown" && netInfo.isInternetReachable === false)
+    return (
+      <View style={styles.container}>
+        <Image style={styles.image} source={require("./assets/offline.png")} />
+        <Text style={styles.text}>No Internet Connection</Text>
+      </View>
+    );
   const uri = Constants?.manifest?.extra?.url;
   return <WebView source={{ uri }} />;
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    width: "100%",
+    zIndex: 1,
+  },
+  image: {
+    height: 500,
+    width: 500,
+  },
+  text: {
+    fontSize: 25,
+  },
+});
+
+export default App;
