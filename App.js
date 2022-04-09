f0d529a79fe021ba0d5da7050ca1d4af9cbd5093
SaraHassanAboEldahab <47996159+SaraHassanAboEldahab@users.noreply.github.com>
2022-04-09 23:14:23 +0200

    handle authentication

--- App.js
+++ App.js
@@ -1,6 +1,42 @@
 import Constants from "expo-constants";
-import { WebView } from "react-native-webview";
-export default function App() {
+import React, { useRef } from 'react';
+import { WebView, WebViewNavigation } from 'react-native-webview';
+import CookieManager from '@react-native-community/react-native-cookies';
+
+const App =()=> {
+  // Send the cookie information back to the mobile app
+const CHECK_COOKIE= `
+ReactNativeWebView.postMessage("Cookie: " + document.cookie);
+true;
+`;
+const onNavigationStateChange = (navigationState) => {
+
+  // Check cookies every time URL changes
+  if (webViewRef.current) {
+    webViewRef.current.injectJavaScript(CHECK_COOKIE);
+  }
+};
+
+const onMessage = async (event => {
+  const { data } = event.nativeEvent;
+
+  if (data.includes('Cookie:')) {
+    // process the cookies
+    const storedCookies = await CookieManager.get(
+      'https://mywebapp.com/login',
+      true
+    );
+}
+})
+
+   const webViewRef = useRef();
   const uri = Constants.manifest.extra.url;
-  return <WebView source={{ uri }} />;
+  return (
+  <WebView source={{ uri }} 
+  ref={webViewRef}
+  onNavigationStateChange={onNavigationStateChange}
+  onMessage={onMessage}
+  sharedCookiesEnabled
+  />
+  );
 }
