50edc58186c33eb9b88d5c099911a6e80a1b2c9c
SaraHassanAboEldahab <47996159+SaraHassanAboEldahab@users.noreply.github.com>
2022-04-09 23:31:46 +0200

    handle authentication

--- App.js
+++ App.js
@@ -1,42 +1,42 @@
 import Constants from "expo-constants";
-import React, { useRef } from 'react';
-import { WebView, WebViewNavigation } from 'react-native-webview';
-import CookieManager from '@react-native-community/react-native-cookies';
+import React, { useRef } from "react";
+import { WebView, WebViewNavigation } from "react-native-webview";
+import CookieManager from "@react-native-community/react-native-cookies";
 
-const App =()=> {
+const App = () => {
+  const webViewRef = useRef();
+  const uri = Constants.manifest.extra.url;
+
   // Send the cookie information back to the mobile app
-const CHECK_COOKIE= `
+  const CHECK_COOKIE = `
 ReactNativeWebView.postMessage("Cookie: " + document.cookie);
 true;
 `;
-const onNavigationStateChange = (navigationState) => {
+  const onNavigationStateChange = (navigationState) => {
+    // Check cookies every time URL changes
+    if (webViewRef.current) {
+      webViewRef.current.injectJavaScript(CHECK_COOKIE);
+    }
+  };
 
-  // Check cookies every time URL changes
-  if (webViewRef.current) {
-    webViewRef.current.injectJavaScript(CHECK_COOKIE);
-  }
+  const onMessage = (event) => {
+    const { data } = event.nativeEvent;
+
+    if (data.includes("Cookie:")) {
+      // process the cookies
+      CookieManager.get(uri, true);
+    }
+  };
+
+  return (
+    <WebView
+      source={{ uri }}
+      ref={webViewRef}
+      onNavigationStateChange={onNavigationStateChange}
+      onMessage={onMessage}
+      sharedCookiesEnabled
+    />
+  );
 };
 
-const onMessage = async (event => {
-  const { data } = event.nativeEvent;
-
-  if (data.includes('Cookie:')) {
-    // process the cookies
-    const storedCookies = await CookieManager.get(
-      'https://mywebapp.com/login',
-      true
-    );
-}
-})
-
-   const webViewRef = useRef();
-  const uri = Constants.manifest.extra.url;
-  return (
-  <WebView source={{ uri }} 
-  ref={webViewRef}
-  onNavigationStateChange={onNavigationStateChange}
-  onMessage={onMessage}
-  sharedCookiesEnabled
-  />
-  );
-}
+export default App;
