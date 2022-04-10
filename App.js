8204b51d3f2a8db50e4d2633b00252889d31360a
Sara <sara.aboeldahab205@gmail.com>
2022-04-10 15:02:06 +0200

    tyr

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
 import Constants from "expo-constants";
 import React, { useRef } from "react";
 import { WebView, WebViewNavigation } from "react-native-webview";
-import CookieManager from "@react-native-community/react-native-cookies";
+import CookieManager from "@react-native-community/cookies";
 
 const App = () => {
-  const webViewRef = useRef();
+  // const webViewRef = useRef();
   const uri = Constants.manifest.extra.url;
 
   // Send the cookie information back to the mobile app
-  const CHECK_COOKIE = `
-ReactNativeWebView.postMessage("Cookie: " + document.cookie);
-true;
-`;
-  const onNavigationStateChange = (navigationState) => {
-    // Check cookies every time URL changes
-    if (webViewRef.current) {
-      webViewRef.current.injectJavaScript(CHECK_COOKIE);
-    }
-  };
+  //   const CHECK_COOKIE = `
+  // ReactNativeWebView.postMessage("Cookie: " + document.cookie);
+  // true;
+  // `;
+  //   const onNavigationStateChange = (navigationState) => {
+  //     // Check cookies every time URL changes
+  //     if (webViewRef.current) {
+  //       webViewRef.current.injectJavaScript(CHECK_COOKIE);
+  //     }
+  //   };
 
   // const onMessage = (event) => {
   //   const { data } = event.nativeEvent;
@@ -34,14 +34,14 @@ true;
     <WebView
       source={{
         uri,
-        headers: {
-          Cookie: CookieManager.get(uri, true),
-        },
+        // headers: {
+        //   Cookie: CookieManager.get(uri, true),
+        // },
       }}
-      ref={webViewRef}
-      onNavigationStateChange={onNavigationStateChange}
+      // ref={webViewRef}
+      // onNavigationStateChange={onNavigationStateChange}
       // onMessage={onMessage}
-      sharedCookiesEnabled
+      // sharedCookiesEnabled
     />
   );
 };
